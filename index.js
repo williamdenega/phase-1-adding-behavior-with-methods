de7f2e2b6c314d5623c8537b615821c7f6e63b0b
WilliamDenega <williamdenega@gmail.com>
2022-02-22 18:29:40 -0500

    commit tree a06a95d916e61ead92a77707d9da8235efa6d24a

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+class Cat {
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird{
+    constructor(name, sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        // if(this.sex === "male") return `It's me! ${this.name}, the parrot!`
+        // return `${this.name} says Squawk!`
+        return (this.sex === "male") ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`
+    }
+
+}
+//return (collection instanceof Array) ? collection.slice() : Object.values(collection);
+
+// bird = new Bird("Pablo", "male")
+// bird2 = new Bird("Mable", "female")
